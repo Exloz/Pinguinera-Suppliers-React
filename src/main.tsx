5f11b31b63b835a15398e6aef8d496fbf83eeb44
exloz <exloz@hotmail.com>
2024-04-19 13:22:59 -0500

    setup general styles and style normalizer

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './app/ui/styles/index.css';
+import './app/ui/styles/normalize.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
