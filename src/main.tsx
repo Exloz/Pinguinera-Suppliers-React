0e00b0dcd9d496c366b91be5fb575bbdb90d8d19
exloz <exloz@hotmail.com>
2024-04-20 11:40:15 -0500

    add general css config and favicon

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import './app/ui/styles/index.css';
 import './app/ui/styles/normalize.css';
+import { App } from './app';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
