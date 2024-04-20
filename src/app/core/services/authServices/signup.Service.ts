cf2781e8a0b2a58fbd2ae16a58a85965f839f383
exloz <exloz@hotmail.com>
2024-04-20 14:04:56 -0500

    create login and signup service and models

---
+++ src/app/core/services/authServices/signup.Service.ts
@@ -0,0 +1,20 @@
+import { ICustomerSignUp } from '../../models/customer.signup';
+import { urls } from '../../resources/url.resource';
+import http from '../general/http.service';
+import { StorageService } from '../general/storage.service';
+
+export const signUpService = (singUpCredentials: ICustomerSignUp): Promise<boolean> => {
+  const url = urls.login;
+  const body = singUpCredentials;
+
+  return http
+    .post(url, body)
+    .then((response) => response.json())
+    .then((response) => {
+      const storage = new StorageService();
+      storage.set('token', response.token);
+      storage.set('email', response.email);
+      storage.set('userName', response.unique_name);
+      return Boolean(response.token);
+    });
+};
